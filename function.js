3001c819c57de139132c1560f2aef0202c38b19c
aimaaan <aimnfauzi98@gmail.com>
2024-04-02 15:46:02 +0800

    clean

--- function.js
+++ function.js
@@ -1,5 +1,4 @@
 function validateForm() {
-    // Simple email validation
     const email = document.getElementById('email').value;
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if (!re.test(email.toLowerCase())) {
